457e4794b0f9775b247b4329964df7a2adbade2e
mai-jay23 <mjzarraga23@gmail.com>
2025-09-30 09:25:11 +0800

    my portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// let firstname = "Mira";
+
+// const address = "Davao";
+// console.log(firstname);
+// console.log(address);
+// let x;
+// for (x = 0; x < 15; x++) {
+//   console.log("hi");
+// }
