641983c549a112a3330d73f56eb47823620b1f4f
mai-jay23 <mjzarraga23@gmail.com>
2025-10-05 21:27:37 +0800

    game nume

--- script.js
+++ script.js
@@ -7,3 +7,45 @@
 // for (x = 0; x < 15; x++) {
 //   console.log("hi");
 // }
+
+// const greet = (name) => `Hello,${name}`;
+// console.log(greet(" Mira"));
+
+// let num1 = 12;
+// let num2 = 3;
+// if (num1 != num2 || num2 ===num1) {
+//   console.log("not equal");
+// } else {
+//   console.log("equal");
+// }
+
+// let name="Mira";
+// let age=22;
+// if (name=="Mira" && age == 22 ) {
+//     console.log("yes,its me");
+// } else {
+//     console.log("not me");
+// if elseif else
+// let userName = "keil4";
+// let password = "icui4cu1";
+
+// if (userName === "keil4" && password === "icui4cu") {
+//   console.log("log in successfully!");
+// } else {
+//   console.log("wrong username or password");
+// }
+// const add=(a,b)=>{ return a+b}
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+// const divide = (a, b) => {
+//   return a / b;
+// };
+
+// console.log(add(1,2));
+// console.log(subtract(1, 2));
+// console.log(multiply(1, 2));
+// console.log(divide(1, 2));
