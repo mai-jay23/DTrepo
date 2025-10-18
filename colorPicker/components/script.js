bb1c9cd98664a3139bf9ce111a3cbe2736838b39
kiel4 <mjzarraga23@gmail.com>
2025-10-18 12:14:23 +0800

    colorPicker

--- colorPicker/components/script.js
+++ colorPicker/components/script.js
@@ -1,6 +1,12 @@
 const color = document.querySelector("button");
 const screenBackground = document.querySelector("body");
+const image = document.getElementById("img");
+const paragraph = document.querySelector("p");
 color.addEventListener("click", () => {
   color.style.backgroundColor = "black";
   screenBackground.style.backgroundColor = "violet";
+  image.src = "./images/stich.webp";
+  image.alt = "stich!";
+
+  paragraph.textContent = "This is stich!";
 });
